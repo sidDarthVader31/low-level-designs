a77b68d92813369d840b759d31ca217e64280065
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-10-12 23:04:22 +0530

    add validation to room

--- meeting-scheduler/go/room.go
+++ meeting-scheduler/go/room.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type Room struct{
   name string
   calendar []Meeting
@@ -9,10 +11,25 @@ type Room struct{
 
 func (room *Room) book(startTime int64, endTime int64) (string, error){
   //add validation
+  isValidBooking, _:= room.isValidBooking(startTime, endTime)
+  if !isValidBooking {
+    return "", errors.New("The room is not available for the scheduled time")  
+  }
   meeting := Meeting {
     startTime: startTime,
     endTime: endTime,
   }
   room.calendar = append(room.calendar, meeting)
   return room.name, nil
+}
+
+
+func(room *Room) isValidBooking(startTime int64, endTime int64) (bool, error) {
+  for _,v := range room.calendar {
+    //check for overlap
+    if (endTime >= v.startTime && endTime <= v.endTime) || (startTime >= v.startTime && startTime <= v.endTime) {
+      return false , nil
+    }
+  }
+  return true, nil;
 }
